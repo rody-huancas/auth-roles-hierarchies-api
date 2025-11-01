60c053fba6420adaee7667fbccd2bca4512c02b1
Rody Huancas <rodyhuancas.04@gmail.com>
2025-10-31 22:16:47 -0500

    Feat: Enhance AppModule: Integrate TypeORM with async configuration and environment variable support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './database/typeorm.config';
+import { UseEnvironmentVariables } from './config/env/env.enable';
 
 @Module({
-  imports: [],
+  imports: [
+    UseEnvironmentVariables,
+    TypeOrmModule.forRootAsync({
+      useFactory: typeOrmConfig,
+      inject    : [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
