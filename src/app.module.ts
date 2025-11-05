bafd1983bb646f0b3d404a6c61d310058735796a
Rody Huancas <rodyhuancas.04@gmail.com>
2025-11-04 21:04:44 -0500

    Refactor: Reorder imports in AppModule for better organization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './database/typeorm.config';
-import { UseEnvironmentVariables } from './config/env/env.enable';
-import { CommonModule } from './common/common.module';
 import { UsersModule } from './modules/users/users.module';
 import { RolesModule } from './modules/roles/roles.module';
+import { CommonModule } from './common/common.module';
+import { typeOrmConfig } from './database/typeorm.config';
 import { OptionsModule } from './modules/options/options.module';
-import { ModuleRegistryModule } from './modules/module-registry/module-registry.module';
 import { AuditLogsModule } from './modules/audit-logs/audit-logs.module';
+import { ModuleRegistryModule } from './modules/module-registry/module-registry.module';
+import { UseEnvironmentVariables } from './config/env/env.enable';
 
 @Module({
   imports: [
